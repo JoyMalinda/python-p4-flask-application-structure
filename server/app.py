9261e6ff6dd4b124f3f9604e69db6d5e5de3783b
Joy Malinda <kokimalinda@gmail.com>
2025-06-10 16:41:45 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to my page!'
+
+@app.route('/<string:username>')  
+def user(username):
+    return f'Profile for {username}'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
